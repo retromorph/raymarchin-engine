86cd246259e6b5137dde34fcba4cb95795ec1da5
mobynick <mobynick365@gmail.com>
2021-09-01 05:22:51 +0300

    Fixes for build automatization

--- raymarchin-api/src/main/kotlin/org/crg/raymarchin/engine/core/render/LightSource.kt
+++ raymarchin-api/src/main/kotlin/org/crg/raymarchin/engine/core/render/LightSource.kt
@@ -5,6 +5,7 @@ import org.crg.raymarchin.engine.dto.RenderDto
 import org.crg.raymarchin.engine.api.render.RayConsumer
 
 import kotlin.math.pow
+import kotlin.streams.toList
 
 class LightSource: RayConsumer {
     override fun getRenderedRay(ray: Ray): List<RenderDto> {
